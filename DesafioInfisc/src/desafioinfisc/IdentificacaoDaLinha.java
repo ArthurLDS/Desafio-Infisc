87260d193787ede723c6b9870fbe8705472bb10c
ArthurLDS <arthurlds73@gmail.com>
2017-09-02 02:16:52 -0300

    feat(IdentificacaoDaLinhaEnum): implementar enum para identificação do tipo de dado lido.

---
+++ DesafioInfisc/src/desafioinfisc/IdentificacaoDaLinha.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desafioinfisc;
+
+/**
+ *
+ * @author Arthur
+ */
+public enum IdentificacaoDaLinha {
+    
+    FUNCIONARIO(0),
+    CLIENTE(1),
+    ATENDIMENTO(2);
+    
+    private int valor;
+    
+    IdentificacaoDaLinha(int numero){
+        valor = numero;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+}
